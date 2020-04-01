3b6f6db1cf611e710b3b5f6e167e1cfd9eda2ca5
wowgarcon <silverdue@gmail.com>
2020-04-01 12:48:46 +0900

    플래너 작성 기능관련 추가 (드래그&드롭, 환율계산기, 계산기)

---
+++ allDoneTrip/src/main/java/org/adt/controller/PlannerController.java
@@ -0,0 +1,50 @@
+package org.adt.controller;
+
+import java.util.HashMap;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+@RequestMapping("/planner/*")
+public class PlannerController {
+	
+	
+	@GetMapping("/write")
+	public void write() {
+		log.info("write");
+	}
+	
+	
+	@PostMapping("/cityInfo")
+	public ResponseEntity<HashMap<String, String>> cityInfo(@RequestParam("img") String img,
+											@RequestParam("title") String title,
+											@RequestParam("text") String text) {
+		
+		if(img != ""){
+			img = ("<img src='/resources/img/"+img+"'>");
+		}
+		if(title != ""){
+			title = ("<div><h5>"+title+"</h5></div>");
+		}
+		if(text != ""){
+			text = ("<div>"+text+"</div>");
+		}
+		
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("img", img);
+		map.put("title", title);
+		map.put("text", text);
+		
+		return new ResponseEntity<>(map, HttpStatus.OK);
+	}
+	
+}
---
+++ allDoneTrip/src/main/java/org/adt/domain/Criteria.java
@@ -0,0 +1,10 @@
+package org.adt.domain;
+
+import lombok.Data;
+
+@Data
+public class Criteria {
+	
+	private int pageNum;
+	private int amount;
+}
