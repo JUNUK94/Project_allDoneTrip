2171afd017553e7fd227f0ac33159f3ac3c74708
naae <naae31@naver.com>
2020-04-02 17:49:38 +0900

    PlannerMapper.xml getList추가

--- allDoneTrip/src/main/java/org/adt/controller/HomeController.java
+++ allDoneTrip/src/main/java/org/adt/controller/HomeController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import lombok.extern.log4j.Log4j;
 /**
  * Handles requests for the application home page.
  */
@@ -25,6 +26,7 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
+		
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
---
+++ allDoneTrip/src/main/java/org/adt/mapper/PlannerMapper.java
@@ -0,0 +1,10 @@
+package org.adt.mapper;
+
+import java.util.List;
+
+import org.adt.domain.PlannerVO;
+
+public interface PlannerMapper {
+
+	public List<PlannerVO> getList();
+}
