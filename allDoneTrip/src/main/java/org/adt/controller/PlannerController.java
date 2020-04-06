ab194b61f8b7da7479f20368cfce0e18829de48a
wowgarcon <silverdue@gmail.com>
2020-04-06 11:16:18 +0900

    CKeditor 적용 1차

--- allDoneTrip/src/main/java/org/adt/controller/PlannerController.java
+++ allDoneTrip/src/main/java/org/adt/controller/PlannerController.java
@@ -36,6 +36,12 @@ public class PlannerController {
 	
 	
 	// 플래너 작성 페이지로 이동
+	@GetMapping("/ckEditorSample")
+	public void ck() {
+	}
+	
+	
+	// 플래너 작성 페이지로 이동
 	@GetMapping("/write")
 	public void write() {
 		log.info("write");
