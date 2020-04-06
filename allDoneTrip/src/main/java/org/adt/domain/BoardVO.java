7608df2f24f9df0a10fc65fa99462cef08f61fd0
aptss <thou5607@gmail.com>
2020-04-06 14:24:06 +0900

    전체 게시판 출력

--- allDoneTrip/src/main/java/org/adt/domain/BoardVO.java
+++ allDoneTrip/src/main/java/org/adt/domain/BoardVO.java
@@ -7,12 +7,15 @@ import lombok.Data;
 @Data
 public class BoardVO {
 
+	private int rn;
 	private int b_Type;
 	private String bType_Name;
 	private int member_No;
 	private int bno;
 	private String title;
 	private String b_content;
+	private String nick_Name;
+	private String email;
 	private int b_clickNum;
 	private int b_Like;
 	private String regdate;
