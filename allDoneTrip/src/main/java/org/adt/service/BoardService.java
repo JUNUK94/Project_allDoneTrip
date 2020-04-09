a2ae0d0d5ffc972e9c4d9d08e4092a6c62b8f738
naae <naae31@naver.com>
2020-04-09 13:06:48 +0900

    게시판 서비스 수정

--- allDoneTrip/src/main/java/org/adt/service/BoardService.java
+++ allDoneTrip/src/main/java/org/adt/service/BoardService.java
@@ -16,29 +16,10 @@ public interface BoardService {
 	//게시판 전체 갯수
 	public int getTotal(Criteria cri);
 	
-	//전체게시판 페이징 처리
-	public int boardPaging(Criteria cri);
-
-	//후기게시판 페이징처리
-	public int reviewWithPaging(Criteria cri);
-	
-	//동행찾기 페이징처리
-	public int friendWithPaging(Criteria cri);
-	
-	//자유게시판 페이징처리
-	public int freeWithPaging(Criteria cri);
-	
 	
 	//게시판글작성
-	public void reviewInsert(BoardVO board);
-	
-	public void reviewInsertSelectKey(BoardVO board);
-	
-	
-	
-	//글검색
-	public BoardVO read(Long bno);
-	
+	public void Insert(BoardVO board);
+
 	//글삭제
 	public int delete(Long bno);
 	
